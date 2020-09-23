9790caf45bd666295115cbd7394505549953bcac
L2az <71586138+L2az@users.noreply.github.com>
2020-09-23 15:13:30 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,6 +6,14 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
+@client.event
+async def on_message(message):
+   if message.author.bot:　　　　　　　　　　　　　　　
+       return  　　　　　　　 #発言者がBOTなら無視
+   if message.content == '樋□さんこんちわ':　　　　 #発言に返信する
+       await message.channel.send('しずかにして') #返信内容は''で括る。
+
+
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
