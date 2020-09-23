8a0d3c1bd2dab6b912ab10c3c26bd5ecab2f0128
L2az <71586138+L2az@users.noreply.github.com>
2020-09-23 15:15:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 import os
 import traceback
-
+import discord 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
@@ -10,7 +10,7 @@ token = os.environ['DISCORD_BOT_TOKEN']
 async def on_message(message):
    if message.author.bot:　　　　　　　　　　　　　　　
        return  　　　　　　　 #発言者がBOTなら無視
-   if message.content == '樋□さんこんちわ':　　　　 #発言に返信する
+   if message.content == 'こんちわ':　　　　 #発言に返信する
        await message.channel.send('しずかにして') #返信内容は''で括る。
 
 
